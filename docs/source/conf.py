0d4db36b2a5a59919b87bcb239e965a24887ce2b
Adam Chyła <adam@chyla.org>
2023-07-29 23:40:44 +0200

    init documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Files Integrity Checker'
+copyright = '2023, Adam Chyła'
+author = 'Adam Chyła'
+release = 'main'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'classic'
+html_static_path = ['_static']
+html_copy_source = False
+html_show_sphinx = False
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Copyright (c) 2023, Adam Chyła <adam@chyla.org>
+
+   This Source Code Form is subject to the terms of the Mozilla Public
+   License, v. 2.0. If a copy of the MPL was not distributed with this
+   file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+Welcome
+=======
+
+
+Get started
+-----------
+
+These sections cover the basics of getting started.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
